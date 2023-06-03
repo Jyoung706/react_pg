bc7156ae9c343867823472cca518d45bc2c7cb92
Jyoung706 <wnsdud9476@gmail.com>
2023-06-04 00:17:42 +0900

    click useState

--- src/AppCounter.jsx
+++ src/AppCounter.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 
 export default function AppCounter() {
+  const [number, setNumber] = useState(0);
+  const handleClick = () => setNumber((prev) => prev + 1);
   return (
     <div>
-      <Counter />
-      <Counter />
+      <div>
+        Total Count : {number} {number > 10 ? "💔" : "❤"}
+      </div>
+      <div>
+        <Counter total={number} onClick={handleClick} />
+        <Counter total={number} onClick={handleClick} />
+      </div>
     </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 
-export default function Counter() {
+export default function Counter({ total, onClick }) {
   const [number, setNumber] = useState(0);
   return (
-    <div className='counter'>
-      <span>{number}</span>
+    <div className="counter">
+      <p>
+        {number} <span>/{total}</span>
+      </p>
       <button
         onClick={() => {
           setNumber((prev) => prev + 1);
+          onClick();
         }}
       >
         Click!
