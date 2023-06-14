57fe2a61ff16fff56a5a5d494a07030fa2136ba5
jYoung706 <wnsdud9476@gmail.com>
2023-06-14 18:45:01 +0900

    mentors name Modify

--- src/AppMentor.jsx
+++ src/AppMentor.jsx
@@ -4,40 +4,47 @@ export default function AppMentor() {
   const [person, setPerson] = useState({
     name: "Jeon",
     title: "Developer",
-    mentor: {
-      name: "Bob",
-      title: "Senior Developer",
-    },
+    mentor: [
+      {
+        name: "Bob",
+        title: "Full Stack Developer",
+      },
+      {
+        name: "James",
+        title: "Backend Developer",
+      },
+    ],
   });
   return (
     <div>
       <h1>
         {person.name}은 {person.title}
       </h1>
-      <p>
-        {person.name}의 멘토는 {person.mentor.name} ({person.mentor.title})
-      </p>
+      <ul>
+        {person.mentor.map((value, index) => (
+          <li key={index}>
+            {value.name} ({value.title})
+          </li>
+        ))}
+      </ul>
       <button
         onClick={() => {
-          const name = prompt("멘토 이름은 무엇입니까?");
+          const prevName = prompt("바꿀 멘토의 이름은?");
+          const name = prompt("무엇으로 바꾸시겠습니까?");
+
           setPerson((prev) => ({
             ...prev,
-            mentor: { ...prev.mentor, name },
+            mentor: prev.mentor.map((mentor) => {
+              if (mentor.name === prevName) {
+                return { ...mentor, name };
+              }
+
+              return mentor;
+            }),
           }));
         }}
       >
         멘토 이름 바꾸기
-      </button>
-      <button
-        onClick={() => {
-          const title = prompt("멘토의 직업은 무엇입니까?");
-          setPerson((prev) => ({
-            ...prev,
-            mentor: { ...prev.mentor, title },
-          }));
-        }}
-      >
-        멘토 타이틀 바꾸기
       </button>
     </div>
   );
